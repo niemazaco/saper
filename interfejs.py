275f91938bb52c0bf32ffb6fd704656b5313bd8e
niemazaco <64072380+niemazaco@users.noreply.github.com>
2020-06-14 18:37:59 +0200

    Add files via upload
    Dodana dokumentacja, zmiana niektórych nazw metod oraz obsługa odkrywania pól przy przegranej.

--- interfejs.py
+++ interfejs.py
@@ -1,6 +1,14 @@
 from tkinter import *
 
 class BladDanychException(Exception):
+    """Zwraca tekst do message boxa przy blednych danych wejsciowych.
+    
+    Klasy pochodne przygotowuja odpowiedni tekst do wyswietlenia w zaleznosci od
+    podanych w inputach parametrow.
+    Lista klas pochodnych:
+    BladSzerokosciException
+    BladWysokosciException
+    BladBombException"""
     def __init__(self, ktorepole, wartosc):
         self._pole = ktorepole
         self._trescbledu = wartosc
@@ -42,26 +50,29 @@ class BladBombException(BladDanychException):
                   "Co ty, chcesz grac bez bomb? Wpisz dodatnia ilosc bomb!")
         
 
-
-
-class interfejs():
+class Interfejs():
+    """Wydzielona czesc programu - odpowiada za tworzenie i aktualizacje interfejsu."""
     def __init__(self,logika):
+        '''Tworzy interfejs umozliwiajacy rozpoczecie gry.
+        
+        Przyjmuje referencje do obiektu Logika, aby mogly zostac wykonane akcje
+        przy wciskaniu przyciskow.'''
         self._logika = logika 
         self._window=Tk()
         self._window.title("Saper Jakub Medalion")
         self._window.geometry("570x400")
         
-        #bindowanie klawiszy do kody xyzzy:
+        #bindowanie klawiszy do kodu xyzzy:
         self._window.bind("x", lambda e:self.xyzzy('x'))
         self._window.bind("y", lambda e:self.xyzzy('y'))
         self._window.bind("z", lambda e:self.xyzzy('z'))
         self._window.bind("X", lambda e:self.xyzzy('x'))
         self._window.bind("Y", lambda e:self.xyzzy('y'))
         self._window.bind("X", lambda e:self.xyzzy('z'))
+        #ilosc znakow z kodu juz wpisana
         self._kodwpisany = 0
         
-        #tla przyciskow
-        #(0- puste, 1- flaga, 2 - pytajnik, 3 - klikniete)
+        #tla przyciskow dla stanow 0-10 wypisanych w klasie Plansza
         self._i0 = PhotoImage(file="pusty.png")
         self._i1 = PhotoImage(file="flaga.png")
         self._i2 = PhotoImage(file="pytajnik.png")
@@ -71,13 +82,12 @@ class interfejs():
         self._flagazla = PhotoImage(file="flagazla.png")
         self._xyzzy0 = PhotoImage(file="xyzzy0.png")
         self._xyzzy1 = PhotoImage(file="xyzzy1.png")
-        self._xyzzy2 = PhotoImage(file="xyzzy2.png")
-        #xyzzy3 == _i3
+        self._xyzzy2 = PhotoImage(file="xyzzy2.png")        
         self._xyzzybomba = PhotoImage(file="xyzzybomba.png")
-        #xyzzy wybuch - nie wystapi
         self._listatel = [self._i0,self._i1,self._i2,self._ibomb,
                           self._i3,self._flagazla,self._iwybuch,
                           self._xyzzy0,self._xyzzy1,self._xyzzy2,self._xyzzybomba]
+        
         # utworzenie glownych kontenerow
         self._panellewy = Frame(self._window, bg='blue', width=160, height=400, padx=3, pady=3)
         self._panelprawy = Frame(self._window, bg='gold', width=400, height=400, padx=3, pady=3)
@@ -105,17 +115,13 @@ class interfejs():
                             self.wczytaj(self._inwys.get(),
                                          self._inszer.get(),
                                          self._inbomb.get()))
-
-        # print(pb.pole_klik(w,k)
         
         self._lflag = Label(self._panellewy, text='pozostalo bomb:')
         self._lpyt = Label(self._panellewy, text='ilosc pytajnikow:')
         self._lwolne = Label(self._panellewy, text='wolne pola:')
-        self._lstatus = Label(self._panellewy, text='WPISUJ I ZACZYNAJ :-)')
-        
+        self._lstatus = Label(self._panellewy, text='WPISUJ I ZACZYNAJ :-)')        
         
         # ukladanie elementow w lewym panelu
-        #model_label.grid(row=0, columnspan=3)
         self._lszer.grid(row=0, padx=3, pady=3)
         self._inszer.grid(row=1, padx=3, pady=3)
         self._lwys.grid(row=2, padx=3, pady=3)
@@ -127,23 +133,21 @@ class interfejs():
         self._lpyt.grid(row=10, padx=3, pady=3)
         self._lwolne.grid(row=11, padx=3, pady=3)
         self._lstatus.grid(row=12, padx=3, pady=3)
-        #panel prawy
-        '''for x in range(15): 
-            self._panelprawy.columnconfigure(x, weight=1)
-            self._panelprawy.rowconfigure(x, weight=1)'''
+        #panel prawy - tworzony w metodzie ustaw_plansze()
         
     def start(self):
+        '''Uruchamia interfejs po zwroceniu referencji do obiektu z konstruktora do klasy Logika.'''
         self._window.mainloop()
         
     #aktualizacja labeli na lewym panelu
-    def ustawinfo(self,flagi,pyt,ilewolnych):
-            #print(bomby,flagi,pyt,ilewolnych)
+    def ustaw_info(self,flagi,pyt,ilewolnych):
+        '''Metoda aktualizujaca labele na lewym panelu.'''
         self._lflag.configure(text='pozostalo bomb: {}'.format(flagi))
         self._lpyt.configure(text='ilosc pytajnikow: {}'.format(pyt))
         self._lwolne.configure(text='wolne pola: {}'.format(ilewolnych))
-            #self._panellewy.update_idletasks()
         
     def wczytaj(self,w,k,b):
+        '''Metoda walidujaca dane wejsciowe przy rozpoczynaniu gry.'''
         try:
             w = int(float(w))
             k = int(float(k))
@@ -168,30 +172,22 @@ class interfejs():
             messagebox.showinfo(title=e.wypisz1(), message=e.wypisz2())
             return
         else:
-            self._logika.utworzplansze(w,k,b)
+            if (self._logika.utworz_plansze(w,k,b)):
+                self.ustaw_plansze(w,k,b)
     
-    def ustawplansze(self,w,k,b):
+    def ustaw_plansze(self,w,k,b):
+        '''Tworzy siatke przyciskow z akcjami przy PPM i LPM.
+        
+        Przyjmuje ilosc wierszy, kolumn i bomb tworzonej planszy.'''
+        
         #lewy panel
-        self.ustawinfo(b-0, 0, w*k-b)
-        #czysci plansze z poprzedniej gry
+        self.ustaw_info(b-0, 0, w*k-b)
+        #czyszczenie planszy z poprzedniej gry
         self._xyzzy = False
+        self._kodwpisany = 0
         for widget in self._panelprawy.winfo_children():
             widget.destroy()
-        
-    
-        #tworzy plansze
-        '''if self._plansza:
-            print(self._plansza[0][0])
-            for i in range(self._pop_w):
-                for j in range (self._pop_k):
-                    self._plansza[i][j].destroy()
-        self._pop_k = k
-        self._pop_w = w    '''
-        '''self._plansza = [[ Button(self._panelprawy,
-                                  command=lambda i=i,j=j: print(i,j),borderwidth=1)
-                          .grid(row=i,column=j, sticky='news') for j in range(k)] for i in range(w)]'''
-    
-        
+        #tworzenie planszy
         self._plansza = [[ Button(self._panelprawy, image=self._i0,
                                   command=lambda i=i,j=j: self._logika.klik(i,j,False),
                                   borderwidth=1, width=26, height=26, compound="center")
@@ -199,44 +195,49 @@ class interfejs():
         #metoda Button.grid zwraca none (dokumentacja) - musialem rozdzielic
         for i in range(w):
             for j in range(k):
-                #self._plansza[i][j].grid(row=i,column=j, sticky='news')
-                self._plansza[i][j].place(x=26*i, y=26*j)
-                #self._plansza[i][j].geometry('26x26')
+                self._plansza[i][j].place(x=26*i, y=26*j) #rozmieszczenie
                 #akcja na PPM
                 self._plansza[i][j].bind('<Button-3>',lambda e,i=i,j=j: self._logika.klik(i,j,True))
-        
-        self._lstatus.configure(text='graj, graj')
-        #print(self._plansza)
-    # print(pb.pole_klik(i,j))
+        self._lstatus.configure(text='graj, graj')    
     
-    
-    
-    def ustawpole(self,w,k,cyfra,stan,ileflag,pyt,ilewolnych):
-        self.ustawinfo(ileflag,pyt,ilewolnych)
-        if stan in [3,4,5,6,10]: #inne LPM - blokujace nastepne kliki
+    def ustaw_pole(self,w,k,cyfra,stan,ileflag,pyt,ilewolnych):
+        '''Aktualizuje lewy panel inform. oraz tlo i liczbe w przycisku.'''
+        #lewy panel
+        self.ustaw_info(ileflag,pyt,ilewolnych)
+        #przy LPM - blokujace nastepne kliki
+        if stan in [3,4,5,6,10]: 
             self._plansza[w][k].configure(text=cyfra, state=DISABLED, width=23, height=23)
             self._plansza[w][k].unbind('<Button-3>')
+        #ustawia odpowiednie tlo przycisku
         self._plansza[w][k].configure(image=self._listatel[stan], width=23, height=23)
         
     def xyzzy(self,klawisz):
+        '''Sprawdza, czy została wcisnieta sekwencja znaków do uruchomienia kodu'''
+        if hasattr(self, '_xyzzy') == False: #gra jeszcze nierozpoczeta
+            return
         if self._xyzzy == False and klawisz == "xyzzy"[self._kodwpisany]:
             self._kodwpisany += 1
             if self._kodwpisany == 5:
-                self._kodwpisany = 0
-                self._logika.prosbaOKod()
-                self._xyzzy = True
+                self._logika.prosba_o_kod()
+                #kod zostal wpisany, blokada wpisywania do rozpoczenia nastepnej partii
+                self._xyzzy = True      
+        else: #wcisnieto inny niz wymagany przycisk (z przyciskow bindowanych)
+            self._kodwpisany = 0
         
     def wygrana(self):
+        '''Wyswietla komunikat o wygranej partii.'''
         self._lstatus.configure(text='WYGRANA :-)')
+        del self._xyzzy
         messagebox.showinfo(title='WYGRANA', message='WYGRANA! :-) Dawaj jeszce raz')
         
     def przegrana(self):
+        '''Wyswietla komunikat o przegranej partii.'''
         self._lstatus.configure(text='PRZEGRANA :-(')
+        del self._xyzzy
         messagebox.showinfo(title='PRZEGRANA', message='PRZEGRANA :-( Dawaj jeszce raz')
 
     
-
-
+#uruchamianie programu
 if __name__ == "__main__":
     from logika import *
     start = Logika()
--- logika.py
+++ logika.py
@@ -1,63 +1,43 @@
-'''
-#list comprehension 3 przyklady (byle pokazać, że się umie)
-1 - plansza 2d - tworzenie macierzy pol
-2 - przyciski 2d - tworzenie macierzy przyciskow pol
-    
-# 3x lambda
-JEST
-1 - przycisk start LPM
-2 - przycisk pola LPM
-3 - bind PPM pola
-4 - bind x,y,z
-
-
-# klasy - 4 wlasne klasy, w tym dwie dziedziczace po innych moich
-
-#klasy - podzial
-
-JEST - podzial na pliki
-
-
-#walidacja wyjatkow - 
-
-JEST
-
-własne moduły - podział na dwa pliki
-JEST
-
-4/5 (wynagane 4)
-
-'''
-
-'''
-do zrobienia na pewno:
-    dokumentacja
-'''
 import interfejs as gui
 import random
+import time
 
 
-class plansza():
+class Plansza():
+    """Przechowuje wszystkie info o planszy i wykonuje na niej dzialania.
+    
+    Jest to podklasa klasy Logika, wydzielenie jej umozliwia szybkie i bezbledne
+    usuwanie starego obiektu i zastapienie go nowym przy restarcie gry.
+    """
     def __init__(self,wier,kol,bomb,logika,gui):
+        """Tworzy macierz reprezentujaca plansze.
+        
+        Przyjmuje wymiary planszy, ilosc bomb oraz referencje do klas Logika 
+        i Interfejs."""
+        #referencje
         self._logika = logika
         self._gui = gui
-        #rozmiar
+        #rozmiar planszy
         self._w = wier #numeracja 1-15
         self._k = kol
         
+        #ilosc bomb
         self._ilebomb = bomb
-        #dodatkowe info
+        # liczniki
         self._wolnepola = wier * kol - bomb
         self._ileflag = 0
         self._flagzbomb = 0
         self._pytajniki = 0
-        self._uzytykod = False #kod xyzzy
+        #czy zostal uzyty kod - potrzebne przy aktualizacji statusow pol
+        self._uzytykod = False
           
-        #utworz pionki [wiersz][kolumna][nr stanu,czy jest bomba]
-        #(0- puste, 1- flaga, 2 - pytajnik, 3 - bomba) - po xyzzy stan+=7
-        #(4 - klikniete, 5 - zla flaga, 6 - wybuch) - tylko bez xyzzy
-        # w sumie 7+4 stanow
-        #
+        '''Tworzenie macierzy pol [wiersz][kolumna][nr stanu, czy jest bomba].
+        Lista stanow pol:
+        0- puste, 1- flaga, 2 - pytajnik, 3 - bomba   // po xyzzy stan+=7
+        4 - klikniete bez bomby                       // tylko bez xyzzy
+        5 - zle postawiona flaga, 6 - wybuch          // tylko bez xyzzy
+        w sumie 7 + 4 = 11 stanow
+        '''
         self._pola = [[[0,False] for j in range(kol)] for i in range(wier)]
         
         #losowanie pol
@@ -69,17 +49,10 @@ class plansza():
                 self._pola[x][y][1] = True
                 licznik -= 1
                 if licznik == 0:
-                    break    
-    
-    def wyslijplansze(self):
-        if self._uzytykod:
-            [[ self._pola[i][j][0] for j in range(self._k) if self._pola[i][j][0] in [0,1,2,3,5,6] ]
-             for i in range(self._w)]
-        #zrobic list comprehension selekcja pol z kodami 0,1,2,3,5,6 + sprawdzic czy xyzzy
-        pass
- 
+                    break
 
-    def wygrana(self):        
+    def wygrana(self):
+        """Autoodkrywanie niewcisnietych pol + zwraca nr statusu 'wygrana'."""        
         #odkrywanie pozostałych pól       
         for w in range(self._w):
             for k in range(self._k):
@@ -89,58 +62,67 @@ class plansza():
                         ile = ''
                     else:
                         self._pola[w][k][0] = 4 #ustawianie stanu
-                        ile = str(self.przegladnijsasiadow(w,k))
+                        ile = str(self.przegladnij_sasiadow(w,k))
                         #uaktualnianie przycisku
                         if ile == '0':
                             ile = ''
-                    self._gui.ustawpole(w, k, ile, self._pola[w][k][0],
-                                        self._ilebomb - self._ileflag, self._pytajniki,
-                                        self._wolnepola)
+                    self._gui.ustaw_pole(w, k, ile, self._pola[w][k][0],0,0,0)
             
         return 1 #status wygranej
     
     def przegrana(self):
-        '''for w in range(self._w):
+        """Sprawdzanie pol + zwraca nr statusu 'przegrana'."""
+        for w in range(self._w):
             for k in range(self._k):
-                if self._pola[w][k][0] %7 in [0,2]:  
+                if self._pola[w][k][0] %7 in [0]:
+                    #nieklikniete pole z bomba
                     if self._pola[w][k][1] == True: #bomba
-                        self._pola[w][k][0] = 3 + self._uzytykod * 7
-                        ile = ''
-                    else:
-                        self._pola[w][k][0] = 4 #ustawianie stanu
-                        ile = str(self.przegladnijsasiadow(w,k))
-                        #uaktualnianie przycisku
-                        if ile == '0':
-                            ile = ''
-                    self._gui.ustawpole(w, k, ile, self._pola[w][k][0],
-                                        self._ilebomb - self._ileflag, self._pytajniki,
+                        self._pola[w][k][0] = 3 + self._uzytykod * 7 #ustawianie stanu
+                        self._gui.ustaw_pole(w, k, '', self._pola[w][k][0],
+                                        self._ilebomb - self._flagzbomb, self._pytajniki,
                                         self._wolnepola)
-                    
-        '''      
+                elif self._pola[w][k][0] %7 in [1]:
+                    #flaga postawiona w zlym miejscu
+                    if self._pola[w][k][1] == False: #bomba
+                        self._gui.ustaw_pole(w, k, '', 5,
+                                        self._ilebomb - self._flagzbomb, self._pytajniki,
+                                        self._wolnepola)
+                elif self._pola[w][k][0] %7 in [2]:
+                    #odkrywanie pytajnika jezeli jest pod nim bomba
+                    if self._pola[w][k][1] == True: #bomba
+                        self._pola[w][k][0] = 3 + self._uzytykod * 7 #ustawianie stanu
+                        self._gui.ustaw_pole(w, k, '', self._pola[w][k][0],
+                                        self._ilebomb - self._flagzbomb, self._pytajniki,
+                                        self._wolnepola)             
         return 2 #status przegranej
        
     #sprawdza pole po kliknieciu LPM
-    def poleLPM(self,w,k,nieklikajflagi=False):
+    def poleLPM(self,w,k,nie_klikaj_pytajnika=False):
+        """Obsluga algorytmu przy wcisnieciu przycisku LPM na polu o wspolrzednych [w,k].
+        
+        Zmienna nie_klikaj_pytajnika uchrania przed autoodkrywaniem pol oznaczonych
+        pytajnikiem, umozliwiajac jego normalne klikniecie LPM w celu odkrycia pola."""
         #pole w pozycji startowej
         if self._pola[w][k][0] % 7 == 0:
                 if self._pola[w][k][1] == True: #bomba
                     self._pola[w][k][0] = 6
+                    self._gui.ustaw_pole(w, k, '', self._pola[w][k][0],
+                                    self._ilebomb - self._ileflag, self._pytajniki,
+                                    self._wolnepola)
                     return self.przegrana()
                 else: 
                     self._pola[w][k][0] = 4 #ustawianie stanu
-                    ile = str(self.przegladnijsasiadow(w,k))
+                    ile = str(self.przegladnij_sasiadow(w,k))
                     #uaktualnianie przycisku
                     if ile == '0':
                         ile = ''
-                        self.przeklikajsasiadow(w,k)
+                        self.przeklikaj_sasiadow(w,k)
                     self._wolnepola -= 1
-                    self._gui.ustawpole(w, k, ile, self._pola[w][k][0],
+                    self._gui.ustaw_pole(w, k, ile, self._pola[w][k][0],
                                     self._ilebomb - self._ileflag, self._pytajniki,
                                     self._wolnepola)
                     if self._wolnepola == 0 == self._pytajniki:
-                        return self.wygrana()
-
-          
+                        return self.wygrana()          
         #pole z flaga
         elif self._pola[w][k][0] % 7 == 1:
             #przyc lewy zablokowany
@@ -150,32 +132,35 @@ class plansza():
         #pole z pytajnikiem
         elif self._pola[w][k][0] % 7 == 2:
             #przy automatycznym odkrywaniu nie sprawdza flagi
-            if nieklikajflagi == False: 
+            if nie_klikaj_pytajnika == False: 
                 self._pytajniki -= 1
                 if self._pola[w][k][1] == True: #bomba
                     self._pytajniki -= 1
                     self._pola[w][k][0] = 6
+                    self._gui.ustaw_pole(w, k, '', self._pola[w][k][0],
+                                    self._ilebomb - self._ileflag, self._pytajniki,
+                                    self._wolnepola)
                     return self.przegrana()
                     
             #klik PPM - z autoodkrywaniem
                 else:
                     self._pola[w][k][0] = 4
-                    ile = str(self.przegladnijsasiadow(w,k))
+                    ile = str(self.przegladnij_sasiadow(w,k))
                     #uaktualnianie przycisku
                     if ile == '0':
                         ile = ''
-                        self.przeklikajsasiadow(w,k)
+                        self.przeklikaj_sasiadow(w,k)
                     self._wolnepola -= 1
-                    self._gui.ustawpole(w, k, ile, self._pola[w][k][0],
+                    self._gui.ustaw_pole(w, k, ile, self._pola[w][k][0],
                                     self._ilebomb - self._ileflag, self._pytajniki,
                                     self._wolnepola)
                     if self._wolnepola == 0 == self._pytajniki:
-                        return self.wygrana()
-        
+                        return self.wygrana()      
         return 0 #status "gramy dalej"
 
     #sprawdza pole po kliknieciu PPM
     def polePPM(self,w,k):
+        """Obsluga algorytmu przy wcisnieciu przycisku PPM na polu o wspolrzednych [w,k]."""
         #pole w pozycji startowej
         if self._pola[w][k][0] % 7 == 0:
                 #ustaw stan z uwzglednieniem kodu xyzzy jesli to jest pole z bomba
@@ -185,7 +170,7 @@ class plansza():
                 if self._pola[w][k][1] == True:
                     self._flagzbomb += 1
                 #ustawianie pola w interfejsie
-                self._gui.ustawpole(w, k, "", self._pola[w][k][0],
+                self._gui.ustaw_pole(w, k, "", self._pola[w][k][0],
                                     self._ilebomb - self._ileflag, self._pytajniki,
                                     self._wolnepola)
                 #sprawdzanie czy nastapila wygrana
@@ -200,7 +185,7 @@ class plansza():
                 self._ileflag -= 1
                 if self._pola[w][k][1] == True:
                     self._flagzbomb -= 1
-                self._gui.ustawpole(w, k, "", self._pola[w][k][0],
+                self._gui.ustaw_pole(w, k, "", self._pola[w][k][0],
                                     self._ilebomb - self._ileflag, self._pytajniki,
                                     self._wolnepola)
                 
@@ -209,7 +194,7 @@ class plansza():
         elif self._pola[w][k][0] % 7 == 2:
                 self._pytajniki -= 1
                 self._pola[w][k][0] = 0+ 7*self._uzytykod*self._pola[w][k][1]
-                self._gui.ustawpole(w, k, "", self._pola[w][k][0],
+                self._gui.ustaw_pole(w, k, "", self._pola[w][k][0],
                                     self._ilebomb - self._ileflag, self._pytajniki,
                                     self._wolnepola)
                 
@@ -225,7 +210,8 @@ class plansza():
 
 
 
-    def przegladnijsasiadow(self,m,n): # zlicza ile bomb wkolo
+    def przegladnij_sasiadow(self,m,n):
+        """Oblicza, z iloma minami sasiaduje pole o wspolrzednych [m,n]."""
         licznik = 0
         if m>0:
             if n>0:
@@ -255,72 +241,71 @@ class plansza():
                     licznik += 1
         return licznik
 
-    def przeklikajsasiadow(self,m,n):  #LPM na pole 0 bomb - przeklikaj wszystkie wkolo
+    def przeklikaj_sasiadow(self,m,n):
+        """Autoodkrywanie sasiednich pol."""
         if m>0:
             if n>0:
-                self.poleLPM(m-1,n-1,nieklikajflagi=True)
-            self.poleLPM(m-1,n,nieklikajflagi=True)
+                self.poleLPM(m-1,n-1,nie_klikaj_pytajnika=True)
+            self.poleLPM(m-1,n,nie_klikaj_pytajnika=True)
             if n<self._k-1:
-                self.poleLPM(m-1,n+1,nieklikajflagi=True)
+                self.poleLPM(m-1,n+1,nie_klikaj_pytajnika=True)
             
         if n>0:
-            self.poleLPM(m,n-1,nieklikajflagi=True)
+            self.poleLPM(m,n-1,nie_klikaj_pytajnika=True)
         if n<self._k-1:
-            self.poleLPM(m,n+1,nieklikajflagi=True)
+            self.poleLPM(m,n+1,nie_klikaj_pytajnika=True)
     
         if m<self._w-1:
             if n>0:
-                self.poleLPM(m+1,n-1,nieklikajflagi=True)
-            self.poleLPM(m+1,n,nieklikajflagi=True)
+                self.poleLPM(m+1,n-1,nie_klikaj_pytajnika=True)
+            self.poleLPM(m+1,n,nie_klikaj_pytajnika=True)
             if n<self._k-1:
-                self.poleLPM(m+1,n+1,nieklikajflagi=True)
+                self.poleLPM(m+1,n+1,nie_klikaj_pytajnika=True)
 
     def xyzzy(self):
-        if self._uzytykod == True:
-            return
+        """Aktualizuje stan pol z bombami na stan po uzyciu kodu."""
         self._uzytykod = True
         for w in range(self._w):
             for k in range(self._k):
                 if self._pola[w][k][0] in [0,1,2,3]:
                     if self._pola[w][k][1] == True: #bomba
                         self._pola[w][k][0] += 7
-                        self._gui.ustawpole(w, k, '', self._pola[w][k][0],
+                        self._gui.ustaw_pole(w, k, '', self._pola[w][k][0],
                                         self._ilebomb - self._ileflag, self._pytajniki,
                                         self._wolnepola)
 
 
-
-
-
-
-
 class Logika():
-    def __init__(self): 
+    """Klasa odpowiadajaca za logike gry.
+    
+    Jej czesc zostala wydzielona do klasy Plansza, aby ja latwo usuwac i tworzyc na nowo."""
+    def __init__(self):
+        '''Definiuje potrzebne zmienne i uruchamia interfejs, ktory przejmuje dzialanie programu.'''
         #blokuje wykonywanie wrazliwych fkcji (kilk buttona) az nie zakonczy sie poprzednie wywolanie
         self._aktualizacja = False   
         #blokada przyciskow po wygranej lub przegranej partii
         self._grazakonczona = False 
         #tworzenie interfejsu bez planszy
-        self._gui = gui.interfejs(self)
+        self._gui = gui.Interfejs(self)
         #wyswietlanie i dzialanie okienka
         self._gui.start()
         
 
  
-    def utworzplansze(self,w,k,b):
+    def utworz_plansze(self,w,k,b):
+        """Usuwa obiekt planszy z poprzedniej partii i wywoluje tworzenie nowego."""
         if self._aktualizacja == True:
-            return
+            return False
         self._aktualizacja = True
         self._grazakonczona = False
         if hasattr(self, '_plansza') == True:
             del self._plansza
-        self._plansza = plansza(w,k,b,self,self._gui) #logika planszy
-        self._gui.ustawplansze(w, k, b) #przyciski w GUI
+        self._plansza = Plansza(w,k,b,self,self._gui) #tworzenie planszy
         self._aktualizacja = False
-        pass
+        return True
     
-    #sprawdza pole po kliknieciu
-    def klik(self,w,k,PPM):       
+    def klik(self,w,k,PPM):
+        """Sprawdza pole po jego kliknieciu i podaje do interfejsu info o koncu gry."""
         if self._aktualizacja == True or self._grazakonczona == True:
             return
         self._aktualizacja = True
@@ -334,20 +319,18 @@ class Logika():
         elif w == 2: #przegrana
             self._grazakonczona = True
             self._gui.przegrana()
-        
-        #przegrana
-        #wyswietlic zawartosc wszystkich pol(przegrana + pokazac stan 2 + xyzzy
-        #+ zle postawione flagi + klikniete pole z bomba)
         self._aktualizacja = False
         pass
     
-    def prosbaOKod(self):
-        if self._aktualizacja == True:
-            return False
+    def prosba_o_kod(self):
+        '''Metoda wywolywana po wcisnieciu sekwencji xyzzy.'''
+        while self._aktualizacja == True:
+            time.sleep(0.05)
+        #sekcja krytyczna
         self._aktualizacja = True
         self._plansza.xyzzy()
         self._aktualizacja = False
-        return True
     
+#uruchamianie programu
 if __name__ == "__main__":
     start = Logika()
